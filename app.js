d0740ff1ef8aa6e9a384f5a6c91dc990dd14fc69
Rarazc <rafael-kinect@live.com>
2025-11-24 22:38:28 -0300

    Criando loop que permanece até que o usuario acerte o numero secreto, e uma estrutura de condição if else para dar dicas se o numero é maior ou menor

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = Math.round(Math.random() * 100);
-alert(numeroSecreto);
+let chute;
+
+while (numeroSecreto != chute){
+  chute = prompt('Escolha um número entre 1 e 100');
+
+  if (chute == numeroSecreto){
+
+  }
+  else if (chute < numeroSecreto){
+    alert('Que pena! tente novamente! o numero é maior que: ' + chute);
+  }
+  else{
+    alert('Que pena! tente novamente! o numero é menor: ' + chute);
+  }
+}
