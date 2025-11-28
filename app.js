69f5df4b9c15467f62700a6854767bdccba2becf
Rarazc <rafael-kinect@live.com>
2025-11-27 21:09:06 -0300

    Realização do Desafio 2 que possui 5 partes, uma delas é uma versão com pontuação do Numero Secreto

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = Math.round(Math.random() * 100);
+let numeroTeto = prompt('Digite o intervalo maximo do jogo: ');
+let numeroSecreto = Math.round(Math.random() * numeroTeto);
 
 // loop do while com break apenas ao acertar o numero
 do{
-  let chute = Number(prompt('Escolha um número entre 1 e 100'));
+  let chute = Number(prompt(`Escolha um número entre 1 e ${numeroTeto}`));
 
   if (chute == numeroSecreto){
       alert(`Isso ai!! parabens voce acertou, o numero secreto era ${numeroSecreto}`);
