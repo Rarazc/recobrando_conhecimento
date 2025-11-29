4333a5e6f2d25b99b1089fea2c32d851351e2ec8
Rarazc <rafael-kinect@live.com>
2025-11-28 21:42:25 -0300

    Consertado estrutura if else e realizado o Desafio 3

---
+++ Desafio3/Desafio3.js
@@ -0,0 +1,32 @@
+//Parte 1
+alert('Parte 1: contador até 10 ');
+let contador = 1;
+while (contador <= 10) {
+    alert(contador);
+    contador++;
+}
+
+//Parte 2
+alert('Parte 2: contador de 10 até 0 ');
+let contador2 = 10;
+while (contador2 >= 0) {
+  alert(contador2);
+    contador2--;
+}
+
+//Parte 3
+alert('Parte 3: contagem regressiva ');
+let numero = Number(prompt('Digite um numero para contagem regressiva'));
+while (numero >= 0) {
+  alert(numero);
+    numero--;
+}
+
+//Parte 4
+alert('Parte 3: contagem progressiva ');
+let numero2 = Number(prompt('Digite um numero para contagem progressiva'));
+let contador3 = 0;
+while (contador3 <= numero2) {
+  alert(contador3);
+  contador3++;
+}
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroTeto = prompt('Digite o intervalo maximo do jogo: ');
 let numeroSecreto = Math.round(Math.random() * numeroTeto);
 let chute = 0;
-let tentativas = 0;
+let tentativas = 1;
 
 while (chute !== numeroSecreto){
   chute = Number(prompt(`Escolha um número entre 1 e ${numeroTeto}`));
@@ -10,11 +10,12 @@ while (chute !== numeroSecreto){
   if (chute === numeroSecreto) {
     alert(`Isso ai!! parabens voce acertou, o numero secreto era ${numeroSecreto}`);
     alert(`Você acertou em ${tentativas} tentativas`);
-  } else if (chute < numeroSecreto) {
-    alert(`Que pena! tente novamente! o numero é maior que: ${chute}`);
-    tentativas++;
   } else {
-    alert(`Que pena! tente novamente! o numero é menor que: ${chute}`);
-    tentativas++;
-  }
+      if (chute < numeroSecreto) {
+        alert(`Que pena! tente novamente! o numero é maior que: ${chute}`);
+      } else {
+          alert(`Que pena! tente novamente! o numero é menor que: ${chute}`);
+        }
+      tentativas++;
+    }
 }
